7dffc0befcbeb898d0762dec6ecf5463e25cf558
Oleg Karpovsky <olegkarp02@mail.ru>
2025-09-26 00:24:05 +0300

    Сделаны первые 2 задания. Для пункта 2.2 раскоментировать пустой массив

--- week2/main.c
+++ week2/main.c
@@ -33,14 +33,53 @@ int FindMaximum(const int* signals, int size) {
     return max;
 }
 
+// Find average value in an array of integers
+double FindAverage(const int* signals, int size) {
+    if (size <= 0) {
+        return INT_MIN;
+    }
+    
+    int sum = 0;
+    double average = 0;
+    for (int i = 0; i < size; i++) {
+        sum += signals[i];
+    }
+
+    average = (double)sum / (double)size;
+
+    return average;
+}
+
+// Find minimum index in an array of integers
+int FindMinimumIndex(const int* signals, int size) {
+    if (size <= 0) {
+        return INT_MIN;
+    }
+    
+    int index = 0;
+    int min = signals[0];
+    for (int i = 1; i < size; i++) {
+        if (signals[i] < min) {
+            min = signals[i];
+            index = i;
+        }
+    }
+    
+    return index;
+}
+
 int main() {
     // Example signal data
     int signals[] = {5, -3, 10, 1, 8, -7, 4, 2};
+    // Для случая пустого массива расскоментировать строку ниже и настроить CMakeLists.txt
+    // int signals[] = {};
     int size = sizeof(signals) / sizeof(signals[0]);
     
     int min_value = FindMinimum(signals, size);
     int max_value = FindMaximum(signals, size);
-    
+    double average_value = FindAverage(signals, size);
+    int min_index = FindMinimumIndex(signals, size);
+
     printf("Signals: ");
     for (int i = 0; i < size; i++) {
         printf("%d ", signals[i]);
@@ -49,6 +88,8 @@ int main() {
     
     printf("Minimum value: %d\n", min_value);
     printf("Maximum value: %d\n", max_value);
-    
+    printf("Average value: %f\n", average_value);
+    printf("Index of minimum value: %d\n", min_index);
+
     return 0;
 }
